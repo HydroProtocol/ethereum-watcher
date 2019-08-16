c0821efba7a94e65b859653a877c6a775d9507c2
Liu Mingmin <liumm@ddex.io>
2019-08-16 20:35:16 +0800

    use ReceiptLogWatcherConfig

--- receipt_log_watcher.go
+++ receipt_log_watcher.go
@@ -2,9 +2,11 @@ package nights_watch
 
 import (
 	"context"
+	"fmt"
 	"github.com/HydroProtocol/nights-watch/rpc"
 	"github.com/HydroProtocol/nights-watch/structs"
 	"github.com/sirupsen/logrus"
+	"sync"
 	"time"
 )
 
@@ -14,9 +16,10 @@ type ReceiptLogWatcher struct {
 	startBlockNum         int
 	contract              string
 	interestedTopics      []string
-	handler               func(from, to int, receiptLog *structs.RemovableReceiptLog)
-	steps                 int
+	handler               func(from, to int, receiptLog *structs.RemovableReceiptLog) error
 	highestSyncedBlockNum int
+	highestSyncedLogIndex int
+	config                ReceiptLogWatcherConfig
 }
 
 func NewReceiptLogWatcher(
@@ -25,9 +28,12 @@ func NewReceiptLogWatcher(
 	startBlockNum int,
 	contract string,
 	interestedTopics []string,
-	handler func(from, to int, receiptLog *structs.RemovableReceiptLog),
-	steps int,
+	handler func(from, to int, receiptLog *structs.RemovableReceiptLog) error,
+	configs ...ReceiptLogWatcherConfig,
 ) *ReceiptLogWatcher {
+
+	config := decideConfig(configs...)
+
 	return &ReceiptLogWatcher{
 		ctx:                   ctx,
 		api:                   api,
@@ -35,29 +41,68 @@ func NewReceiptLogWatcher(
 		contract:              contract,
 		interestedTopics:      interestedTopics,
 		handler:               handler,
-		steps:                 steps,
 		highestSyncedBlockNum: startBlockNum - 1,
+		config:                config,
 	}
+}
+
+func decideConfig(configs ...ReceiptLogWatcherConfig) ReceiptLogWatcherConfig {
+	var config ReceiptLogWatcherConfig
+	if len(configs) == 0 {
+		config = defaultConfig
+	} else {
+		config = configs[0]
+
+		if config.IntervalForPollingNewBlockInSec <= 0 {
+			config.IntervalForPollingNewBlockInSec = defaultConfig.IntervalForPollingNewBlockInSec
+		}
+
+		if config.StepSizeForBigLag <= 0 {
+			config.StepSizeForBigLag = defaultConfig.StepSizeForBigLag
+		}
+
+		if config.RPCMaxRetry <= 0 {
+			config.RPCMaxRetry = defaultConfig.RPCMaxRetry
+		}
+	}
+
+	return config
+}
+
+type ReceiptLogWatcherConfig struct {
+	StepSizeForBigLag               int
+	ReturnForBlockWithNoReceiptLog  bool
+	IntervalForPollingNewBlockInSec int
+	RPCMaxRetry                     int
+	LagToHighestBlock               int
+	StartSyncAfterLogIndex          int
+}
+
+var defaultConfig = ReceiptLogWatcherConfig{
+	StepSizeForBigLag:               50,
+	ReturnForBlockWithNoReceiptLog:  false,
+	IntervalForPollingNewBlockInSec: 15,
+	RPCMaxRetry:                     5,
+	LagToHighestBlock:               0,
+	StartSyncAfterLogIndex:          0,
 }
 
 func (w *ReceiptLogWatcher) Run() error {
 
 	var blockNumToBeProcessedNext = w.startBlockNum
-	defer func() {
-		w.updateHighestSyncedBlockNum(blockNumToBeProcessedNext - 1)
-	}()
+	//defer func() {
+	//	w.updateHighestSyncedBlockNumAndLogIndex(blockNumToBeProcessedNext - 1)
+	//}()
 
-	rpc := rpc.NewEthRPCWithRetry(w.api, 5)
+	rpc := rpc.NewEthRPCWithRetry(w.api, w.config.RPCMaxRetry)
 
 	for {
 		select {
 		case <-w.ctx.Done():
-			//w.updateHighestSyncedBlockNum(blockNumToBeProcessedNext - 1)
 			return nil
 		default:
 			highestBlock, err := rpc.GetCurrentBlockNum()
 			if err != nil {
-				//w.updateHighestSyncedBlockNum(blockNumToBeProcessedNext - 1)
 				return err
 			}
 
@@ -67,15 +112,17 @@ func (w *ReceiptLogWatcher) Run() error {
 
 			numOfBlocksToProcess := int(highestBlock) - blockNumToBeProcessedNext + 1
 			if numOfBlocksToProcess <= 0 {
-				logrus.Debugf("no new block after %d, sleep 3 seconds", highestBlock)
-				time.Sleep(3 * time.Second)
+				sleepSec := w.config.IntervalForPollingNewBlockInSec
+
+				logrus.Debugf("no new block after %d, sleep %d seconds", highestBlock, sleepSec)
+				time.Sleep(time.Duration(sleepSec) * time.Second)
 				continue
 			}
 
 			var to int
-			if numOfBlocksToProcess > w.steps {
+			if numOfBlocksToProcess > w.config.StepSizeForBigLag {
 				// quick mode
-				to = blockNumToBeProcessedNext + w.steps - 1
+				to = blockNumToBeProcessedNext + w.config.StepSizeForBigLag - 1
 			} else {
 				// normal mode, 1block each time
 				to = blockNumToBeProcessedNext
@@ -83,31 +130,67 @@ func (w *ReceiptLogWatcher) Run() error {
 
 			logs, err := rpc.GetLogs(uint64(blockNumToBeProcessedNext), uint64(to), w.contract, w.interestedTopics)
 			if err != nil {
-				//w.updateHighestSyncedBlockNum(blockNumToBeProcessedNext - 1)
 				return err
 			}
 
-			if len(logs) == 0 {
-				// report progress
-				w.handler(blockNumToBeProcessedNext, to, nil)
+			if len(logs) == 0 && w.config.ReturnForBlockWithNoReceiptLog {
+				err := w.handler(blockNumToBeProcessedNext, to, nil)
+				if err != nil {
+					logrus.Infof("err when handling nil receipt log, block range: %d - %d", blockNumToBeProcessedNext, to)
+					return fmt.Errorf("NIGHTS_WATCH handler(nil) returns error: %s", err)
+				}
+
+				w.updateHighestSyncedBlockNumAndLogIndex(to, -1)
 			} else {
 				for i := 0; i < len(logs); i++ {
-					w.handler(blockNumToBeProcessedNext, to, &structs.RemovableReceiptLog{
-						IReceiptLog: logs[i],
+					log := logs[i]
+
+					if log.GetBlockNum() == w.startBlockNum && log.GetLogIndex() <= w.highestSyncedLogIndex {
+						logrus.Debugf("log(%d-%d) < %d-%d, skipped",
+							log.GetBlockNum(), log.GetLogIndex(), w.startBlockNum, w.highestSyncedLogIndex,
+						)
+
+						continue
+					}
+
+					err := w.handler(blockNumToBeProcessedNext, to, &structs.RemovableReceiptLog{
+						IReceiptLog: log,
 					})
+
+					if err != nil {
+						logrus.Infof("err when handling receipt log, block range: %d - %d, receipt: %+v",
+							blockNumToBeProcessedNext, to, log,
+						)
+
+						return fmt.Errorf("NIGHTS_WATCH handler returns error: %s", err)
+					}
+
+					w.updateHighestSyncedBlockNumAndLogIndex(log.GetBlockNum(), log.GetLogIndex())
 				}
 			}
 
-			w.updateHighestSyncedBlockNum(to)
 			blockNumToBeProcessedNext = to + 1
 		}
 	}
 }
 
-func (w *ReceiptLogWatcher) updateHighestSyncedBlockNum(num int) {
-	w.highestSyncedBlockNum = num
+var progressLock = sync.Mutex{}
+
+func (w *ReceiptLogWatcher) updateHighestSyncedBlockNumAndLogIndex(block int, logIndex int) {
+	progressLock.Lock()
+	defer progressLock.Unlock()
+
+	w.highestSyncedBlockNum = block
+	w.highestSyncedLogIndex = logIndex
 }
 
 func (w *ReceiptLogWatcher) GetHighestSyncedBlockNum() int {
 	return w.highestSyncedBlockNum
+}
+
+func (w *ReceiptLogWatcher) GetHighestSyncedBlockNumAndLogIndex() (int, int) {
+	progressLock.Lock()
+	defer progressLock.Unlock()
+
+	return w.highestSyncedBlockNum, w.highestSyncedLogIndex
 }
